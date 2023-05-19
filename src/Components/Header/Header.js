6ddc870065b5cb07628191090951d3e983826699
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-05-18 22:00:10 -0400

    Complete navbar Styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,18 @@
-import  { Component } from 'react';
+import React, { Component } from 'react';
 import './Header.css';
+
 class Header extends Component {
     render() {
         return (
             <div className='navbar'>
-              <ul>
-                    <li><a href='#home'>Home</a></li>
-                    <li><a href='#about'>About</a></li>
-                    <li><a href='#projects'>Projects</a></li>
-              </ul>
-
-               
+                <ul>
+                    <li><a href="#Home">Home</a></li>
+                    <li><a href="#About">About</a></li>
+                    <li><a href="#Projects">Projects</a></li>
+                    <li><a href="#Contact">Contact</a></li>
+                </ul>
             </div>
-        )
+        );
     }
 }
 
