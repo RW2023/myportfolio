dfe03bdc667adbe201e0fbf0d83d6e38d494481c
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-05-19 03:48:55 -0400

    create and render projects component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './Components/Header/Header';
+import Projects from './Components/Projects/Projects';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Projects />
     </div>
   );
 }
