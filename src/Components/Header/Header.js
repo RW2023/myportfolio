5bbc787afe6cbf600f31e3dcbfa4da74d341142e
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-05-20 02:49:31 -0400

    Did a lot of stuff
    All I know is everyting is working at this point. Only need to get navbar smaller on the mobile version.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,6 +7,11 @@ class Header extends Component {
     render() {
         return (
             <div className='navbar'>
+                <div className='logo' id='selfie'>
+                    <img src='https://scontent-yyz1-1.xx.fbcdn.net/v/t1.18169-9/20258048_10155252709870860_3685633259436970279_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Nwdzq2vyT3sAX8yn8Fu&_nc_ht=scontent-yyz1-1.xx&oh=00_AfDLVy0JaXgub0EV5HoGxx9iLWN6J3xc8Tz1qub20rybKQ&oe=648FE34B' alt="Selfie" /> 
+                    
+
+                    </div> 
                 <ul>
                     <li><a href="#Home">Home</a></li>
                     <li><a href="#About">About</a></li>
