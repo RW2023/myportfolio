bdf7dcd194d02a29d126be0cb234512ba4f4bb5e
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-05-20 18:05:07 -0400

    fix navbar links

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -13,10 +13,10 @@ class Header extends Component {
 
                     </div> 
                 <ul>
-                    <li><a href="#Home">Home</a></li>
-                    <li><a href="#About">About</a></li>
-                    <li><a href="#Projects">Projects</a></li>
-                    <li><a href="#Contact">Contact</a></li>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
                 </ul>
                
             </div>
