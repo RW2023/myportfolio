f267fb4ee3d13194cf5ddc4d74201c553abb3df1
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-05-20 22:40:04 -0400

    temporarily comment out contact

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -16,7 +16,7 @@ class Header extends Component {
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#projects">Projects</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    {/* <li><a href="#contact">Contact</a></li> */}
                 </ul>
                
             </div>
