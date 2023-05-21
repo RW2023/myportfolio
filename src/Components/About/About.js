174ac8bf434085792549a199dfc3fe142058315a
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-05-20 22:19:34 -0400

    update and fix links

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -19,12 +19,13 @@ class About extends React.Component {
                 <div id='links'>
                     <h3>Connect With Me</h3>
                     <ul className="social">
-                        <li><a href="https://www.linkedin.com/in/ryan-wilson-2023/" target='_blank' rel="noreferrer"><i class="fa-brands fa-linkedin"></i></a>LinkedIn</li>
-                        <li><a href="instagram.com/ryanwilson2023" target='_blank' rel="noreferrer"><i class="fa-brands fa-instagram"></i></a>Instagram</li>
-                        <li><a href="github.com/RW2023" target='_blank' rel="noreferrer"><i class="fa-brands fa-github"></i></a>GitHub</li>
-                        <li><a href="https://twitter.com/RyanWi2023" target='_blank' rel="noreferrer"><i class="fa-brands fa-twitter"></i></a>Twitter</li>
+                        <li><a href="https://www.linkedin.com/in/ryan-e-wilson/" target='_blank' rel="noreferrer"><i className="fab fa-linkedin"></i></a>LinkedIn</li>
+                        <li><a href="https://instagram.com/ryanwilsonimages" target='_blank' rel="noreferrer"><i className="fab fa-instagram"></i></a>Instagram</li>
+                        <li><a href="https://github.com/RW2023" target='_blank' rel="noreferrer"><i className="fab fa-github"></i></a>GitHub</li>
+                        <li><a href="https://twitter.com/ryanwilson2009" target='_blank' rel="noreferrer"><i className="fab fa-twitter"></i></a>Twitter</li>
                     </ul>
                 </div>
+
             </div>
         )
     }
