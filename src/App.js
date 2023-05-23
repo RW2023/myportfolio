8781ed39b78c39f66d71f71b630b91972e3918d2
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-05-23 09:10:14 -0400

    Render Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import About from './Components/About/About';
+import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Projects from './Components/Projects/Projects';
 
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <About />
       <Projects />
+      <Footer />
     </div>
   );
 }
