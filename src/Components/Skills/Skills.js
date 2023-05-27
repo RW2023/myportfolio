4cc33a499ea948c67a9a030f482e1deb4f04a7d0
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-05-27 15:30:42 -0400

    create and style 'Skills' component

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -3,8 +3,17 @@ import './Skills.css';
 class Skills extends Component {
     render() {
         return (
-            <div>
+            <div className='Skills'>
                 <h2>Skills</h2>
+                <div className='Skills_icon'>
+                    <ul>
+                        <i class="fa-brands fa-html5"></i><p>HTML</p>
+                        <i class="fa-brands fa-css3"></i><p>CSS</p>
+                        <i class="fa-brands fa-square-js"></i><p>JavaScript</p>
+                        <i class="fa-brands fa-react"></i><p>React</p>
+                        <i class="fa-brands fa-node-js"></i><p>NodeJS</p>
+                    </ul>
+                </div>
             </div>
         )
     }
