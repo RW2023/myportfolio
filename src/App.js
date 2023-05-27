f6383c148f8bee37aa202c23dd556775cb3e602a
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-05-27 12:09:14 -0400

    Create and Render Skill Component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import About from './Components/About/About';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Projects from './Components/Projects/Projects';
+import Skills from './Components/Skills/Skills';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <About />
+      <Skills />
       <Projects />
       <Footer />
     </div>
---
+++ src/Components/Skills/Skills.js
@@ -0,0 +1,13 @@
+import react, { Component } from 'react';
+import './Skills.css';
+class Skills extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Skills</h2>
+            </div>
+        )
+    }
+}
+
+export default Skills;
