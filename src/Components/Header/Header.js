4748d6482242d2e82d982eceadfc6ae3cbbb947d
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-05-28 19:06:16 -0400

    remove footer nav item

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,17 +8,18 @@ class Header extends Component {
         return (
             <div className='navbar'>
                 <div className='logo' id='selfie'>
-                    <img src='https://scontent-yyz1-1.xx.fbcdn.net/v/t1.18169-9/20258048_10155252709870860_3685633259436970279_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Nwdzq2vyT3sAX8yn8Fu&_nc_ht=scontent-yyz1-1.xx&oh=00_AfDLVy0JaXgub0EV5HoGxx9iLWN6J3xc8Tz1qub20rybKQ&oe=648FE34B' alt="Selfie" /> 
-                    
+                    <img src='https://scontent-yyz1-1.xx.fbcdn.net/v/t1.18169-9/20258048_10155252709870860_3685633259436970279_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Nwdzq2vyT3sAX8yn8Fu&_nc_ht=scontent-yyz1-1.xx&oh=00_AfDLVy0JaXgub0EV5HoGxx9iLWN6J3xc8Tz1qub20rybKQ&oe=648FE34B' alt="Selfie" />
 
-                    </div> 
+
+                </div>
                 <ul>
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
+                    <li><a href="#skills">Skills</a></li>
                     <li><a href="#projects">Projects</a></li>
-                    {/* <li><a href="#contact">Contact</a></li> */}
+
                 </ul>
-               
+
             </div>
         );
     }
