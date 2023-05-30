4aa00af570f80aa77d31956087741c3016fd481a
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-05-30 17:01:13 -0400

    add links to header and connect

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -17,7 +17,7 @@ class Header extends Component {
                     <li><a href="#about">About</a></li>
                     <li><a href="#skills">Skills</a></li>
                     <li><a href="#projects">Projects</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#links">Links</a></li>
 
                 </ul>
 
